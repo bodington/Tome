eb435d56e9305939d6cba7b475f35250a88f0496
Dylan Bodington <bodington@gmail.com>
2021-12-20 10:51:02 +0000

    Update predTopt.py
    Included both standard deviation and standard error of the optimal temperature.

--- tome/core/predTopt.py
+++ tome/core/predTopt.py
@@ -209,8 +209,8 @@ def main(args):
             pred_topt = pred_fasta_topt(infile,inogt)
             with open(outfile, 'w') as f:
                 dfAsString = pred_topt.to_string(header=True, index=False)
-                f.write('Filename\tTopt Min\tTopt Max\tTopt Range\tSE\n')
-                f.write('{0}\t{1}\t{2}\t{3}\t{4}\n\n'.format(args.fasta, pred_topt['Topt'].min(), pred_topt['Topt'].max(), pred_topt['Topt'].max() - pred_topt['Topt'].min(), pred_topt['Topt'].sem()))
+                f.write('Filename\tTopt Min\tTopt Max\tTopt Range\tSD\tSE\n')
+                f.write('{0}\t{1}\t{2}\t{3}\t{4}\n\n'.format(args.fasta, pred_topt['Topt'].min(), pred_topt['Topt'].max(), pred_topt['Topt'].max() - pred_topt['Topt'].min(), pred_topt['Topt'].std, pred_topt['Topt'].sem()))
                 f.write(dfAsString)
 
         else: sys.exit('Please provide a list with OGT values')
